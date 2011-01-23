29ec76c97892a53c77480b4d05802f6df953a8f7
fonsinchen <ulfonk_mennhar@gmx.de>
2011-01-23 14:21:24 +0100

    some refactoring in preparation for smallmap-overlay

--- src/linkgraph_gui.h
+++ src/linkgraph_gui.h
@@ -13,6 +13,7 @@
 #define LINKGRAPH_GUI_H_
 
 #include "company_func.h"
+#include "station_base.h"
 
 /**
  * Properties of a link between two stations.
@@ -29,28 +30,31 @@ struct LinkProperties {
 
 /**
  * Handles drawing of links into some window.
- * @tparam Twindow window type to be drawn into. Must provide methods:
- * - Point GetStationMiddle(Station *st) const
+ * @tparam Twindow window type to be drawn into. Must provide "Point GetStationMiddle(const Station *st) const"
+ * @tparam Twidget_id ID of widget in window to be drawn into.
  */
-template<class Twindow>
+template<class Twindow, uint Twidget_id>
 class LinkGraphOverlay {
 public:
+	typedef LinkGraphOverlay<Twindow, Twidget_id> Self;
+
 	static const uint8 LINK_COLOURS[];
 
 	/**
 	 * Create a link graph overlay for the specified window.
 	 * @param w Window to be drawn into.
 	 */
-	LinkGraphOverlay(const Twindow *w, uint wid, uint32 cargo_mask = 0xFFFF,
+	LinkGraphOverlay(const Twindow *w, uint32 cargo_mask = 0xFFFF,
 			uint32 company_mask = (1 << _local_company) | INVALID_COMPANY) :
-			window(w), widget_id(wid), cargo_mask(cargo_mask), company_mask(company_mask)
+			window(w), cargo_mask(cargo_mask), company_mask(company_mask)
 	{}
 
 	void Draw() const;
+	void SetCargoMask(uint32 cargo_mask) {this->cargo_mask = cargo_mask;}
+	void SetCompanyMask(uint32 company_mask) {this->company_mask = company_mask;}
 
 protected:
 	const Twindow *window; ///< Window to be drawn into.
-	uint widget_id;        ///< ID of widget in window to be drawn into.
 	uint32 cargo_mask;     ///< Bitmask of cargos to be displayed.
 	uint32 company_mask;   ///< Bitmask of companies to be displayed.
 
